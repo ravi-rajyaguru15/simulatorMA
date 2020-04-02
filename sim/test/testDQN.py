5ad2e4054e3781243b5404ad0ac00b28f6559ee5
Alwyn Burger <alwyn.burger@uni-due.de>
2020-04-02 15:32:52 +0200

    extended testdqn

--- sim/test/testDQN.py
+++ sim/test/testDQN.py
@@ -42,8 +42,8 @@ exp1 = SimpleSimulation(agentClass=lazyTableAgent)
 if not exp1.sharedAgent.loadModel():
 	print("Training table agent")
 	constants.CENTRALISED_LEARNING = True
-	exp1.setBatterySize(1e-2)
-	for i in range(1):
+	exp1.setBatterySize(1e-1)
+	for i in range(int(1e5)):
 		exp1.simulateEpisode()
 	print("Saving...")
 	exp1.sharedAgent.saveModel()
@@ -52,27 +52,15 @@ exp1.sharedAgent.setProductionMode()
 exp2 = SimpleSimulation(agentClass=lazyDeepAgent)
 exp2.sharedAgent.setProductionMode()
 
-# print(exp2.sharedAgent.systemState.stateCount)
-
 quiet = True
 numIterations = 2000
-# index = 0
-# for i in range(2): # exp1.currentSystemState.getUniqueStates()):
-# 	print(i, exp1.currentSystemState.fromIndex(i).currentState, exp1.currentSystemState.fromIndex(i).dictRepresentation)
-# 	pass
 
 totest = int(exp1.currentSystemState.getUniqueStates())
 for j in range(numIterations):
 	for i in range(totest):
-	# for i in range(exp1.currentSystemState.getUniqueStates() * numIterations):
-		# for j in range(100):
-		# generate index
-		# index = i
-		# index = random.randint(0, exp1.currentSystemState.getUniqueStates() - 1)
 		index = random.randint(0, totest - 1)
 
 		sys.stdout.write("\rProgress: %.2f%%" % ((totest * j + (i+1)) / (totest * numIterations) * 100.))
-		# sys.stdout.write("\rProgress: %.2f%%" % ((i+1) / (totest) * 100.))
 		beforeState = exp1.currentSystemState.fromIndex(index).currentState
 
 		action = random.randint(0, exp1.sharedAgent.numActions-1)
